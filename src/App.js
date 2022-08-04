284bf11388a813fd5f6c6f5afa477f7d74058c93
Keshav Kumar <keshavkumar9431@gmail.com>
2022-08-04 16:46:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import BasicTable from './BasicTable';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicTable />
+    </div>
+  );
+}
+
+export default App;
